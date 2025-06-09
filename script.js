95905f108a0e818725f9dd5d3c9918ee43eaaeef
q965786x <kasatka74@yandex.ru>
2025-06-09 22:50:30 +0700

    Сделала 4-ю домашку по JS

---
+++ script.js
@@ -0,0 +1,10 @@
+function  isNotEven(number) {
+    if (number % 2 === 0) {
+        return 'Число четное';
+    } else {
+        return 'Число нечетное';
+    }
+ }
+
+ console.log(isNotEven(2));
+ console.log(isNotEven(3));
