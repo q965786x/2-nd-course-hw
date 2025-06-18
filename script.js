90415ed0b12afa3bb417e9e29b196b93682fb617
q965786x <kasatka74@yandex.ru>
2025-06-19 00:39:03 +0700

    Создана четвертая игра "Викторина", игра связана с сайтом.

--- script.js
+++
@@ -1,8 +0,0 @@
-function reverseText(str) {
-  return str.split('').reverse().join('');
-}
-
-const textInput = prompt('Давай поиграем! Введите любой текст, и я переверну его!');
-const reversedText = reverseText(textInput);
-
-alert(reversedText);
