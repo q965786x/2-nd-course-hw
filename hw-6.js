0081365ab631cc4224b14b137ccf3049c8d975a1
q965786x <kasatka74@yandex.ru>
2025-06-25 22:20:47 +0700

    Внесла дополнения в 6-ю домашку по JS после просмотра разбора домашних заданий

--- hw-6.js
+++ hw-6.js
@@ -4,6 +4,13 @@
 const str = 'js';
 console.log(str.toUpperCase());
 
+//Вариант 2
+
+let str = 'js';
+str = str.toUpperCase();
+console.log(str);
+
+
 /*Задание 2*/
 //Создать функцию, которая принимает массив строк и строку. 
 //Функция должна вернуть новый массив, содержащий только те элементы первого массива, 
@@ -25,6 +32,31 @@ function getFilteredArray(array, string) {
 
 console.log(getFilteredArray(array, string));
 
+//Вариант 2
+
+function filter(arr, str) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].toLowerCase().startsWith(str.toLowerCase())) {
+            result.push(arr[i]);
+        }
+    }
+
+    return result;
+}
+
+console.log(filter(['JavaScript', 'Java', 'Python'], 'ja'));
+
+//Вариант 3
+
+function filter(arr, str) {
+    const lowerStr = str.toLowerCase()
+    return arr.filter(item => item.toLowerCase().startsWith(lowerStr))
+}
+
+console.log(filter(['JavaScript', 'Java', 'Python'], 'ja'));    
+
+
 /*Задание 3*/
 //Округлить число 32.58884:
 
@@ -37,6 +69,7 @@ let num = 32.58884;
 console.log(Math.floor(num)); //До ближайшего меньшего целого.
 console.log(Math.ceil(num)); //До ближайшего большего целого.
 console.log(Math.round(num)); //До ближайшего целого.
+
 
 /*Задание 4*/
 //Найти минимальное и максимальное значения из чисел 52, 53, 49, 77, 21, 32 
@@ -67,6 +100,7 @@ function getRandomNumber() {
 
 getRandomNumber();
 
+
 /*Задание 6*/
 //Написать функцию, которая принимает целое число и 
 //возвращает массив случайных чисел от 0 до этого числа.
@@ -83,6 +117,28 @@ function getRandomArray(num) {
 console.log(getRandomArray(10));
 console.log(getRandomArray(5)); 
 
+//Вариант 2
+
+function getRandomNumber(num) {
+    let arr = [];
+    for (let i = 0; i < num; i++) {
+        arr.push(Math.floor(Math.random() * num));
+    }
+
+    return arr;
+}
+
+console.log(getRandomNumber(10));
+
+//Вариант 3
+
+function getRandomNumber(num) {
+    return Array.from({ length:Math.floor(num / 2) }, () => Math.floor(Math.random() * num));
+}
+
+console.log(getRandomNumber(10));
+
+
 /*Задание 7*/
 //Создать функцию, которая принимает два целых числа и
 //возвращает случайное число в этом диапазоне.
@@ -97,10 +153,11 @@ function getRandomNumberInRange(min, max) {
 
 console.log(getRandomNumberInRange(1, 10));
 
+
 /*Задание 8*/
 //Вывести в консоль текущую дату.
 
-//Вариант 3
+//Вариант 1
 
 const days = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
 const months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
@@ -131,6 +188,12 @@ console.log(fullDate);
 let currentDate = new Date(2025, 5, 73, 23, 45, 30, 10);
 console.log(currentDate);
 
+//Вариант 2
+
+let currentDate = new Date();
+currentDate.setDate(currentDate.getDate() + 73);
+console.log(currentDate);
+
 /*Задание 10*/
 //Написать функцию, которая принимает дату и возвращает ее в формате:
 //Дата: [число] [месяц на русском] [год] — это [день недели на русском].
@@ -147,4 +210,23 @@ console.log(fullDate);
 console.log(fullTime);
 
 
+//Вариант 2
+
+function formatDate(date) {
+    const days = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+    const months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+    const year = date.getFullYear();
+    const day = date.getDate();
+    const month = date.getMonth();
+    const dayOfWeek = days[date.getDay()]; // Исправлено: getDay() вместо getDate()
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    return `
+    Дата: ${day} ${months[month]} ${year} - это ${dayOfWeek}.
+    Время: ${hours}:${minutes}:${seconds}`;
+}
+
+console.log(formatDate(new Date()));
 
