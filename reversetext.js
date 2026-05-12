ac7c6fb7568cfb97338e0a51f59943eba00f44a6
q965786x <kasatka74@yandex.ru>
2026-05-12 21:37:40 +0700

    Удалены файлы приложения mini-games.

--- reversetext.js
+++
@@ -1,5 +0,0 @@
-function reverseText() {
-  let textInput = String(prompt('Введи любой текст, а я его переверну'));
-  let reversedText = textInput.split('').reverse().join('');
-  alert(reversedText);
-}
